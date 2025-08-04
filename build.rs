0f6eedce747372b49130569d60eb2a36fdb0599a
yemiancheng <ymc.github@gmail.com>
2025-08-04 09:31:51 +0800

    build(core): use cross platform note

--- build.rs
+++ build.rs
@@ -1,7 +1,9 @@
+#[cfg(target_os = "windows")]
 use winres;
 /// use winres to set program file icon in window
 ///
 /// use "icon.ico" in project root
+#[cfg(target_os = "windows")]
 fn set_program_file_icon() {
     let mut res = winres::WindowsResource::new();
     res.set_icon("icon.ico");
@@ -9,6 +11,10 @@ fn set_program_file_icon() {
     res.compile().unwrap();
 }
 
+#[cfg(not(target_os = "windows"))]
+fn set_program_file_icon() {
+    // No icon setting needed for non-Windows platforms
+}
 fn main() {
     set_program_file_icon();
 }
